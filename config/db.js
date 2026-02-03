b514e0eaa3025bbc4d0f27e955601977464590f2
Prathmesh Suhagpure <prathmeshsuhagpure3@gmail.com>
2026-02-03 19:52:04 +0530

    fixed Mongo Error

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
   } catch (error) {
-    console.error(error);
+    console.error('❌ MongoDB connection failed:', error.message);
     process.exit(1);
   }
 };

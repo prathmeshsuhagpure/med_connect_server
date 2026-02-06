ff2930b0b964eb468e794b4c3309cfed917d44ed
Prathmesh Suhagpure <prathmeshsuhagpure3@gmail.com>
2026-02-06 22:37:12 +0530

    Made major changes

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -301,10 +301,12 @@ const signup = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const userData = sanitizeUserData({
       name,
       email,
-      password,
+      password : hashedPassword,
       role,
       phoneNumber,
       address,
